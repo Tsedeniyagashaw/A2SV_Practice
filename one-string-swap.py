622f698fe5198dd63a305da13bf70c532e13b4cf
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-02-05 20:01:52 +0300

    one-string-swap

--- one-string-swap.py
+++ one-string-swap.py
@@ -3,12 +3,12 @@ class Solution:
         if s1 == s2:
             return True
             
-        diffs = []
+        dif = []
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                diffs.append(i)
-        if len(diffs) != 2:
+                dif.append(i)
+        if len(dif) != 2:
             return False
             
-        i, j = diffs
+        i, j = dif
         return s1[i] == s2[j] and s1[j] == s2[i]
