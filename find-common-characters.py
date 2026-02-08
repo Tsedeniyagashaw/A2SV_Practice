1de2f5be93b97b290d5535c2cdae896c26c5435c
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-02-08 19:51:57 +0300

    find-common-characters

---
+++ find-common-characters.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        word = Counter(words[0])
+        for i in words:
+            word &= Counter(i) 
+        return list(word.elements())
