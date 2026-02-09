373819b49d25be14c2fd375835ad0d65f35825b4
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-02-09 21:22:50 +0300

    build-array-from-permutation

---
+++ build-array-from-permutation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        new_arr = []
+        for num in nums:
+            new_arr.append(nums[num])
+        return new_arr    
+
+        
