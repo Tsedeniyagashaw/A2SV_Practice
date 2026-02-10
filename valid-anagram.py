f02f1cb4f2f2d7a5431a5122fa96607becdaa31b
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-02-10 13:39:51 +0300

    valid-anagram

---
+++ valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sorted =sorted(s)
+        t_sorted=sorted(t)
+
+        if len(s) != len(t):
+            return False
+            
+
+        for i in range(len(t)):           
+            if s_sorted[i] != t_sorted[i]:
+             return False
+        return True     
+
+              
+        
