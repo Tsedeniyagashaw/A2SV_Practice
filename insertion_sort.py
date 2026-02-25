e428d10a484d53f8e6912751de5c0de41ecc447a
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-02-25 21:24:55 +0300

    insertion-sort

---
+++ insertion_sort.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def insertionSort1(n, arr):
+        key = arr[n - 1]
+        i = n - 2
+        
+        while i >= 0 and arr[i] > key:
+            arr[i + 1] = arr[i]
+            i -= 1
+            print(*arr)
+        arr[i+1] = key
+        print(*arr)    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
