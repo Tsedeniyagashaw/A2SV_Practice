e9d904ac505e6b3c55a7b67454488883a5b9a389
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-03-05 22:48:40 +0300

    move-zeroes

---
+++ move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+       
+        for num in nums:
+            if num == 0:
+                nums.remove(num)
+                nums.append(num)
+        print(nums)        
