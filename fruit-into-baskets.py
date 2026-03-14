d7ef9c06a4f117485d8f246ac60690d5dc73fa61
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-03-14 16:24:20 +0300

    fruit-into-baskets

---
+++ fruit-into-baskets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count = defaultdict(int)
+        l = 0
+        total = 0
+        res = 0
+        for r in range(len(fruits)):
+            count[fruits[r]] += 1
+            total += 1           
+
+            while len(count) > 2:
+                f = fruits[l]
+                count[f] -= 1
+                total -= 1
+                l += 1
+                if count[f] == 0:
+                    count.pop(f)
+
+            res = max(res, total)
+        return res    
