4a3fcfba9b5184e00d3e05a5ab0d7fba888f10f4
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-03-14 12:15:31 +0300

    3sum-closest

---
+++ 3sum-closest.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        closest_sum = float('-inf')
+        n = len(nums)
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = n - 1  
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                if abs(curr_sum - target) < abs(closest_sum - target):
+                    closest_sum  = curr_sum
+                if curr_sum == target:
+                    return curr_sum
+                elif curr_sum < target:   
+                    left += 1
+                else:
+                    right -= 1
+        return closest_sum            
