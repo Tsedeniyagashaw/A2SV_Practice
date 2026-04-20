1c99f7fee5cdc1f3504bdcac1dcaee7f2d38d031
Tsedeniya Gashaw <tsedeniyagashaw672@gmail.com>
2026-04-20 18:32:04 +0300

    maximum-width-ramp

--- leetcode/maximum-width-ramp_trial3.py
+++ leetcode/maximum-width-ramp_trial3.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
         n = len(nums)
-        stack = [1]
+        stack = [0]
 
         for i in range(1,n):
             if nums[i] < nums[stack[-1]]:
